0d6ffd9af2c6f69e24568aba8fef08beb291d357
PatrykSzymonMlynczak <patrykszymonmlynczak@gmail.com>
2021-04-08 11:43:42 +0200

    .circleci cfg

--- src/main/java/com/example/demo/exceptions/StartDateIsAfterEndDateException.java
+++ src/main/java/com/example/demo/exceptions/StartDateIsAfterEndDateException.java
@@ -3,6 +3,6 @@ package com.example.demo.exceptions;
 public class StartDateIsAfterEndDateException extends RuntimeException {
 
     public StartDateIsAfterEndDateException(String dateStart, String dateEnd) {
-         super("Wrong date order. Start date : " + dateStart +" must be earlier then end date: "+ dateEnd);
+         super("Wrong date order. Start date : " + dateStart +" must be earlier than end date: "+ dateEnd);
     }
 }
